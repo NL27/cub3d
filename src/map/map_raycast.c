7c63cfc5cfb7879208fc4f1e0c7d68777eceea4b
enijakow <enijakow@student.42heilbronn.de>
2022-03-24 14:23:30 +0100

    Implemented a basic raycasting algorithm for the map

---
+++ src/map/map_raycast.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_raycast.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enijakow <enijakow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 14:02:13 by enijakow          #+#    #+#             */
+/*   Updated: 2022/03/24 14:22:52 by enijakow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <math.h>
+
+#include "../include/cub_map.h"
+
+typedef struct s_rayvars
+{
+	int		x_pos;
+	int		y_pos;
+	int		step_x;
+	int		step_y;
+	t_fl	ray_dir_x;
+	t_fl	ray_dir_y;
+	t_fl	side_dist_x;
+	t_fl	side_dist_y;
+	t_fl	delta_dist_x;
+	t_fl	delta_dist_y;
+}	t_rayvars;
+
+static void	map_set_rayvars(t_rayvars *vars, t_vec2_and_angle pos)
+{
+	vars->x_pos = (int) pos.vec.x;
+	vars->y_pos = (int) pos.vec.y;
+	vars->ray_dir_x = cos(pos.angle);
+	vars->ray_dir_y = -sin(pos.angle);
+	//vars->delta_dist_x = sqrt(1 + (vars->ray_dir_y * vars->ray_dir_y) / (vars->ray_dir_x * vars->ray_dir_x));
+	//vars->delta_dist_y = sqrt(1 + (vars->ray_dir_x * vars->ray_dir_x) / (vars->ray_dir_y * vars->ray_dir_y));
+	vars->delta_dist_x = fabs(1.0f / vars->ray_dir_x);
+	vars->delta_dist_y = fabs(1.0f / vars->ray_dir_y);
+	if (vars->ray_dir_x < 0)
+	{
+		vars->step_x = -1;
+		vars->side_dist_x = (pos.vec.x - vars->x_pos) * vars->delta_dist_x;
+	}
+	else
+	{
+		vars->step_x = 1;
+		vars->side_dist_x = (vars->x_pos + 1.0f - pos.vec.x) * vars->delta_dist_x;
+	}
+	if (vars->ray_dir_y < 0)
+	{
+		vars->step_y = -1;
+		vars->side_dist_y = (pos.vec.y - vars->y_pos) * vars->delta_dist_y;
+	}
+	else
+	{
+		vars->step_y = 1;
+		vars->side_dist_y = (vars->y_pos + 1.0f - pos.vec.y) * vars->delta_dist_y;
+	}
+}
+
+static bool	is_hit(t_block block)
+{
+	return (block == BLOCK_NOTHING || block_is_solid(block));
+}
+
+static bool	map_raycast_core(t_map *map, t_vec2i *hit, t_rayvars vars)
+{
+	bool	side;
+	
+	side = false;
+	while (!is_hit(map_at(map, vars.x_pos, vars.y_pos)))
+	{
+		if (vars.side_dist_x < vars.side_dist_y)
+		{
+			vars.side_dist_x += vars.delta_dist_x;
+			vars.x_pos += vars.step_x;
+			side = false;
+		}
+		else
+		{
+			vars.side_dist_y += vars.delta_dist_y;
+			vars.y_pos += vars.step_y;
+			side = true;
+		}
+	}
+	hit->x = vars.x_pos;
+	hit->y = vars.y_pos;
+	return (true);
+}
+
+bool	map_raycast(t_map *map, t_vec2_and_angle pos, t_vec2i *hit)
+{
+	t_rayvars	vars;
+
+	map_set_rayvars(&vars, pos);
+	return (map_raycast_core(map, hit, vars));
+}
