c3e2f36fac9994e8ec8694132a4c9c6f8fcdb11f
Nils Lenoch <nlenoch@2-A-6.42heilbronn.de>
2022-03-30 17:53:39 +0200

    Uncommented one line

--- src/gfx/screen/screen_blit.c
+++ src/gfx/screen/screen_blit.c
@@ -6,7 +6,7 @@
 /*   By: nlenoch <nlenoch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 15:07:13 by enijakow          #+#    #+#             */
-/*   Updated: 2022/03/30 17:19:38 by nlenoch          ###   ########.fr       */
+/*   Updated: 2022/03/30 17:53:05 by nlenoch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	screen_blit(t_screen *screen)
 {
-	// mlx_put_image_to_window(screen->mlx, screen->window, screen->image, 0, 0);	
+	mlx_put_image_to_window(screen->mlx, screen->window, screen->image, 0, 0);	
 }
