a666cde54e090fa2d1e51a35fa8974e421215990
enijakow <enijakow@student.42heilbronn.de>
2022-04-01 14:03:15 +0200

    Removed floor casting

--- src/gfx/screen/screen_render.c
+++ src/gfx/screen/screen_render.c
@@ -6,23 +6,17 @@
 /*   By: enijakow <enijakow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 15:35:08 by enijakow          #+#    #+#             */
-/*   Updated: 2022/04/01 13:23:53 by enijakow         ###   ########.fr       */
+/*   Updated: 2022/04/01 14:02:44 by enijakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub_gfx.h"
 
-void	screen_draw_slice(t_screen *screen, t_vec2_and_angle pos, t_fl x_fisheye, int x, int yup, int ydown, t_fl x_fact, t_rgb color)
+void	screen_draw_slice(t_screen *screen, int x, int yup, int ydown, t_rgb color)
 {
-	const t_fl	fov = 45.0f * (M_PI / 180.0f);
 	int			y;
 	t_fl		y_fact;
-	t_fl		y_angle;
-	t_fl		dist;
-	t_fl		floor_x;
-	t_fl		floor_y;
 
-	(void) x_fact;
 	y = 0;
 	while (y < (int) screen_get_height(screen))
 	{
@@ -33,21 +27,7 @@ void	screen_draw_slice(t_screen *screen, t_vec2_and_angle pos, t_fl x_fisheye, i
 			screen_put(screen, (unsigned int) x, (unsigned int) y, color);
 		}
 		else
-		{
-			//screen_put(screen, (unsigned int) x, (unsigned int) y, RGB_BLACK);
-			y_angle = ((y / (t_fl) screen_get_height(screen)) - 0.5f) * fov;
-			dist = (tan(y_angle + M_PI / 2) * 0.5 / x_fisheye);
-			if (y >= (int) screen_get_height(screen) / 2)
-			{
-				floor_x = cos(pos.angle) * dist - pos.vec.x;
-				floor_y = -sin(pos.angle) * dist - pos.vec.y;
-			}
-			t_fl floor_x2 = floor_x;
-			t_fl floor_y2 = floor_y;
-			floor_x = floorf(floor_x);
-			floor_y = floorf(floor_y);
-			screen_put(screen, (unsigned int) x, (unsigned int) y, rgb_create(fabs(floor_x2) * 255, 0, fabs(floor_y2) * 255));
-		}
+			screen_put(screen, (unsigned int) x, (unsigned int) y, RGB_BLACK);
 		y = y + 1;
 	}
 }
@@ -69,7 +49,7 @@ void	screen_render(t_screen *screen, t_map *map, t_vec2_and_angle pos)
 		// TODO, FIXME, XXX: hit.dist being zero!
 		hit.dist = hit.dist;
 		wall_height = (1 / (hit.dist)) * screen_get_height(screen) / cos(((x / (t_fl) screen_get_width(screen)) - 0.5f) * fov);
-		screen_draw_slice(screen, pos, cos(((x / (t_fl) screen_get_width(screen)) - 0.5f) * fov), x, screen_get_height(screen) / 2 - wall_height / 2, screen_get_height(screen) / 2 + wall_height / 2, hit.tex_x, ((hit.hit_block_x % 2) == (hit.hit_block_y % 2)) ? RGB_GRAY : RGB_WHITE);
+		screen_draw_slice(screen, x, screen_get_height(screen) / 2 - wall_height / 2, screen_get_height(screen) / 2 + wall_height / 2, ((hit.hit_block_x % 2) == (hit.hit_block_y % 2)) ? RGB_GRAY : RGB_WHITE);
 		x = x + 1;
 	}
 }
