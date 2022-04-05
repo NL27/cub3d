26b120bdb09db05cac599f159e330a1eb7507b44
enijakow <enijakow@student.42heilbronn.de>
2022-04-05 18:25:47 +0200

    Implemented map_render_portal

--- src/map/map_render_portals.c
+++ src/map/map_render_portals.c
@@ -6,7 +6,7 @@
 /*   By: enijakow <enijakow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:27:10 by enijakow          #+#    #+#             */
-/*   Updated: 2022/04/05 18:10:28 by enijakow         ###   ########.fr       */
+/*   Updated: 2022/04/05 18:25:24 by enijakow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,8 @@ t_vec2	vec_rotate(t_vec2 v, t_fl alpha)
 	return (result);
 }
 
-void	map_render_portals(t_map *map, t_vec2_and_angle player)
+void	map_render_portal(t_map *map, t_screen* screen, t_vec2_and_angle view, unsigned int index)
 {
-	unsigned int		index;
 	unsigned int		next_index;
 	t_vec2_and_angle	pos;
 	t_vec2				dist;
@@ -34,35 +33,39 @@ void	map_render_portals(t_map *map, t_vec2_and_angle player)
 	t_fl				alpha;
 	t_fl				beta;
 	t_clip				clip;
+
+	next_index = (index + 1) % CUB_PORTAL_COUNT;
+	if (map->portals[index].dir == D_NORTH || map->portals[index].dir == D_SOUTH)
+		alpha = ((3 / 4.0f) * M_PI * 2.0f) - direction_as_angle(map->portals[index].dir);
+	else
+		alpha = ((1 / 4.0f) * M_PI * 2.0f) - direction_as_angle(map->portals[index].dir);
+	beta = direction_as_angle(map->portals[index].dir) - direction_as_angle(map->portals[next_index].dir) + M_PI;
+	dist.x = (view.vec.x - (map->portals[index].x + 0.5f));
+	dist.y = (view.vec.y - (map->portals[index].y + 0.5f));
+	target = vec_rotate(dist, alpha);
+	render_pos = vec_rotate(dist, beta);
+	mov.x = -1;
+	mov.y = 0;
+	mov = vec_rotate(mov, -direction_as_angle(map->portals[index].dir));
+	pos.vec.x = map->portals[next_index].x + mov.x + 0.5f + render_pos.x;
+	pos.vec.y = map->portals[next_index].y + mov.y + 0.5f + render_pos.y;
+	pos.angle = direction_as_angle(map->portals[next_index].dir);
+	clip.direction = map->portals[next_index].dir;
+	if (map->portals[next_index].dir == D_NORTH || map->portals[next_index].dir == D_SOUTH)
+		clip.limit = map->portals[next_index].y;
+	else if (map->portals[next_index].dir == D_EAST || map->portals[next_index].dir == D_WEST)
+		clip.limit = map->portals[next_index].x;
+	screen_render(screen, map, pos, &clip, target);
+}
+
+void	map_render_portals(t_map *map, t_vec2_and_angle player)
+{
+	unsigned int		index;
 	
-	(void) player;
 	index = 0;
 	while (index < CUB_PORTAL_COUNT)
 	{
-		next_index = (index + 1) % CUB_PORTAL_COUNT;
-		if (map->portals[index].dir == D_NORTH || map->portals[index].dir == D_SOUTH)
-			alpha = ((3 / 4.0f) * M_PI * 2.0f) - direction_as_angle(map->portals[index].dir);
-		else
-			alpha = ((1 / 4.0f) * M_PI * 2.0f) - direction_as_angle(map->portals[index].dir);
-		beta = direction_as_angle(map->portals[index].dir) - direction_as_angle(map->portals[next_index].dir) + M_PI;
-		dist.x = (player.vec.x - (map->portals[index].x + 0.5f));
-		dist.y = (player.vec.y - (map->portals[index].y + 0.5f));
-		target = vec_rotate(dist, alpha);
-		render_pos = vec_rotate(dist, beta);
-		mov.x = -1;
-		mov.y = 0;
-		mov = vec_rotate(mov, -direction_as_angle(map->portals[index].dir));
-		pos.vec.x = map->portals[next_index].x + mov.x + 0.5f + render_pos.x;
-		pos.vec.y = map->portals[next_index].y + mov.y + 0.5f + render_pos.y;
-		pos.angle = direction_as_angle(map->portals[next_index].dir);
-		clip.direction = map->portals[next_index].dir;
-		if (map->portals[next_index].dir == D_NORTH || map->portals[next_index].dir == D_SOUTH)
-			clip.limit = map->portals[next_index].y;
-		else if (map->portals[next_index].dir == D_EAST || map->portals[next_index].dir == D_WEST)
-			clip.limit = map->portals[next_index].x;
-		//target.x = (player.vec.x - (map->portals[index].x + 0.5f)) / 2.0f;
-		//target.y = (player.vec.y - (map->portals[index].y + 0.5f)) / 2.0f;
-		screen_render(&map->portals[index].screen, map, pos, &clip, target);
+		map_render_portal(map, &map->portals[index].screen, player, index);
 		index++;
 	}
 }
