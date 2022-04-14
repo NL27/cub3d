973f8d62bab3daa8e276336c4b8693d21d6eeb13
nijakow <79372954+nijakow@users.noreply.github.com>
2022-04-14 14:26:03 +0200

    Implemented portal shoot raycast

--- src/cub/cub_shoot_portal.c
+++ src/cub/cub_shoot_portal.c
@@ -1,6 +1,10 @@
 #include "../include/cub.h"
+#include "../include/cub_map.h"
 
 void	cub_shoot_portal(t_cub *cub)
 {
-	(void) cub;
+	t_hit	hit;
+
+	map_raycast(&cub->map, cub->pos, &hit, NULL, false);
+	printf("%d %d\n", hit.hit_block_x, hit.hit_block_y);
 }
