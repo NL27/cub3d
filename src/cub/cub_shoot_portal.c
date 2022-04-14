311a02c77d5b86f202a44995cd20541710af9a97
nijakow <79372954+nijakow@users.noreply.github.com>
2022-04-14 14:29:30 +0200

    Implemented portal shooting

--- src/cub/cub_shoot_portal.c
+++ src/cub/cub_shoot_portal.c
@@ -3,8 +3,14 @@
 
 void	cub_shoot_portal(t_cub *cub)
 {
-	t_hit	hit;
+	t_hit		hit;
+	t_portal	*portal;
+	static unsigned int	pnum = 0;
 
 	map_raycast(&cub->map, cub->pos, &hit, NULL, false);
-	printf("%d %d\n", hit.hit_block_x, hit.hit_block_y);
+	portal = &cub->map.portals[pnum];
+	pnum = (pnum + 1) % CUB_PORTAL_COUNT;
+	portal->x = hit.hit_block_x;
+	portal->y = hit.hit_block_y;
+	portal->dir = hit.direction;
 }
