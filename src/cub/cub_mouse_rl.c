24e9baac044e9f245311481ce34f2208eaa029de
Nils Lenoch <nlenoch@2-A-6.42heilbronn.de>
2022-04-19 14:17:31 +0200

    Norm

--- src/cub/cub_mouse_rl.c
+++ src/cub/cub_mouse_rl.c
@@ -6,7 +6,7 @@
 /*   By: nlenoch <nlenoch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 19:28:45 by nlenoch           #+#    #+#             */
-/*   Updated: 2022/04/19 14:08:43 by nlenoch          ###   ########.fr       */
+/*   Updated: 2022/04/19 14:13:35 by nlenoch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ void	cub_mouse_rl(int x, int y, t_cub *cub)
 	(void) y;
 	if (cub->mouse_locked)
 	{
-		mlx_mouse_get_pos(cub->gfx.window, &cub->mouse.new_x, &cub->mouse.new_y);
+		mlx_mouse_get_pos(cub->gfx.window,
+			&cub->mouse.new_x, &cub->mouse.new_y);
 		mlx_mouse_move(cub->gfx.window, cub->mouse.old_x, cub->mouse.old_y);
-		cub->pos.angle -= (t_fl)((cub->mouse.new_x - cub->mouse.old_x) / 1500.0f);
+		cub->pos.angle -= (t_fl)((cub->mouse.new_x
+					- cub->mouse.old_x) / 1500.0f);
 	}
 }
